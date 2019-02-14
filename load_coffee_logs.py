e0f10441e18e5f5b772a5711d44d5dddf70ae6aa
Wilson Lui <wl2522@columbia.edu>
2019-02-14 08:00:27 -0500

    added additional parameters to config.yml and replaced individual config variables with a config parameter dictionary

--- load_coffee_logs.py
+++ load_coffee_logs.py
@@ -6,17 +6,14 @@ from boxsdk import Client, JWTAuth
 
 with open('config.yml') as config_file:
     config = yaml.load(config_file)
-    USER_ID = config['user_id']
-    LOCAL_FNAME = config['local_fname']
-    AUTH_FNAME = config['auth_fname']
 
 # Load the latest log file exported to Box from the coffee.guru app
 fname = 'coffee_guru_log_{}.csv'.format(date.today().strftime('%d%m%Y'))
 
 # Authenticate using JWTAuth credentials stored in a JSON file
-sdk = JWTAuth.from_settings_file(AUTH_FNAME)
+sdk = JWTAuth.from_settings_file(config['auth_fname'])
 client = Client(sdk)
-user = client.user(user_id='190932855')
+user = client.user(user_id=str(config['user_id']))
 
 # Search for the log file by its file name using the Box search API
 log_search = client.as_user(user).search()
@@ -27,10 +24,10 @@ search_results = log_search.query(fname,
 log_id = search_results.next()['id']
 
 # Download the search results
-with open(LOCAL_FNAME, mode='wb') as log_path:
+with open(config['local_fname'], mode='wb') as log_path:
     client.as_user(user).file(log_id).download_to(log_path)
 
-logs = pd.read_csv(LOCAL_FNAME, sep=';')
+logs = pd.read_csv(config['local_fname'], sep=';')
 logs['Coffee'] = logs['Coffee'].str.replace(' g', '')
 
 # Split the "Note" column into separate columns on the "/" delimiter
@@ -41,17 +38,17 @@ notes = notes.replace(to_replace=r'\s{2,}', value='', regex=True)
 notes.columns = ['Bean', 'Grind', 'Flavor', 'Balance']
 logs = logs.drop('Note', axis=1)
 logs = pd.concat([logs, notes], axis=1)
-logs.to_csv('coffee_guru_log.csv', index=False)
+logs.to_csv(config['local_fname'], index=False)
 
 conn = sqlite3.connect('coffee_guru.db')
 
-with open('create_coffee_tables.sql') as create_statement:
+with open(config['create_script']) as create_statement:
     conn.executescript(create_statement.read())
     conn.commit()
 
 logs.to_sql('raw_logs', con=conn, if_exists='replace', index=False)
 
-with open('load_coffee_logs.sql') as insert_statement:
+with open(config['insert_script']) as insert_statement:
     conn.execute(insert_statement.read())
     conn.commit()
 
