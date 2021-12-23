29205688eb52a9eb85c1b9e492144a0715dea3ba
Wilson Lui <wl2522@columbia.edu>
2021-12-22 21:30:16 -0500

    refactored the code into separate functions

--- load_coffee_logs.py
+++ load_coffee_logs.py
@@ -1,56 +1,120 @@
 import sqlite3
 import pandas as pd
-from datetime import date
 from yaml import load, SafeLoader
 from boxsdk import Client, JWTAuth
 
 with open('config.yml') as config_file:
     config = load(config_file, Loader=SafeLoader)
 
-# Load the latest log file exported to Box from the coffee.guru app
-fname = '{}_{}.csv'.format(config['local_fname'],
-                           date.today().strftime('%d%m%Y'))
 
 # Authenticate using JWTAuth credentials stored in a JSON file
 sdk = JWTAuth.from_settings_file(config['auth_fname'])
-client = Client(sdk)
-user = client.user(user_id=str(config['app_user_id']))
+session_client = Client(sdk)
+app_user = session_client.user(user_id=str(config['app_user_id']))
 
-# Search for the log file by its file name using the Box search API
-log_search = client.as_user(user).search()
-search_results = log_search.query(fname,
-                                  result_type='file',
-                                  file_extensions=['csv'])
+def download_file(client, user):
+    """Search the Box folder for the log file that should be uploaded daily.
 
-# Download the search results using the log file ID
-log_id = search_results.next()['id']
+    Note: Since the search() method returns inexact matches, the name of the
+    search result file must be compared with the expected file name before
+    continuing.
 
-with open(config['local_fname'], mode='wb') as log_path:
-    client.as_user(user).file(log_id).download_to(log_path)
+    Returns
+    -------
+    logs : pandas DataFrame
+        The coffee brewing log data downloaded from the Box folder
+    log_id : str or int
+        The file ID that was assigned to the log file by Box
 
-logs = pd.read_csv(config['local_fname'], sep=';')
-logs['Coffee'] = logs['Coffee'].str.replace(' g', '')
+    """
+    # Load the latest log file exported to Box from the coffee.guru app
+    fname = '{}_{}.csv'.format(config['local_fname'].replace('.csv', ''),
+                               pd.to_datetime('today').strftime('%d%m%Y'))
 
-# Split the "Note" column into separate columns on the "/" delimiter
-notes = logs['Note']
-notes = notes.str.replace('(Bean:)|(Grind:)|(Flavor:)|(Balance:)', '')
-notes = notes.str.split(r'\s*\/\s*', expand=True)
-notes = notes.replace(to_replace=r'\s{2,}|^\s|\s$', value='', regex=True)
-notes.columns = ['Bean', 'Grind', 'Flavor', 'Balance']
-logs = logs.drop('Note', axis=1)
-logs = pd.concat([logs, notes], axis=1)
-logs.to_csv(config['local_fname'], index=False)
+    # Search for the log file by its file name using the Box search API
+    log_search = client.as_user(user).search()
+    search_results = log_search.query(fname,
+                                      result_type='file',
+                                      file_extensions=['csv'])
+    log_id = search_results.next()['id']
+    result_fname = client.as_user(user).file(log_id).get().name
+    upload_time = client.as_user(user).file(log_id).get().created_at
+    upload_time = pd.to_datetime(upload_time, utc=True).tz_convert('EST')
 
-conn = sqlite3.connect(config['db_name'])
+    logs = pd.read_csv(config['local_fname'], sep=';')
 
-with open(config['create_script']) as create_statement:
-    conn.executescript(create_statement.read())
-    conn.commit()
+    return logs, log_id
 
-logs.to_sql('raw_logs', con=conn, if_exists='replace', index=False)
 
-with open(config['insert_script']) as insert_statement:
-    conn.execute(insert_statement.read())
-    conn.commit()
+def update_table(logs):
+    """Update the local SQLite3 database with the data downloaded from Box.
 
-conn.close()
+    Since the coffee.guru app only contains a "notes" field rather than
+    specific fields for describing different aspects of the coffee, the notes
+    field contains data in the following format:
+
+    "Bean: <coffee bean name> / Grind: <grind setting> /
+     Flavor: <coffee flavor description> Balance: <coffee balance description>"
+
+    Therefore, the column associated with this field needs to be parsed
+    and split into separate columns.
+
+    Note: The coffee logs table uses the brew_date column as the primary key.
+    Existing records with the same brew date as a record that's being inserted
+    will be deleted and overwritten with the new record.
+
+    Parameters
+    ----------
+    logs : pandas DataFrame
+
+    Returns
+    -------
+    None
+
+    """
+    logger = logging.getLogger(__name__ + '.update_table')
+
+    logs['Coffee'] = logs['Coffee'].str.replace(' g', '')
+
+    # Split the "Note" column into separate columns on the "/" delimiter
+    notes = logs['Note']
+    notes = notes.str.replace('(Bean:)|(Grind:)|(Flavor:)|(Balance:)', '')
+    notes = notes.str.split(r'\s*\/\s*', expand=True)
+    notes = notes.replace(to_replace=r'\s{2,}|^\s|\s$', value='', regex=True)
+    notes.columns = ['Bean', 'Grind', 'Flavor', 'Balance']
+    logs = logs.drop('Note', axis=1)
+    logs = pd.concat([logs, notes], axis=1)
+    logs.to_csv(config['local_fname'], index=False)
+
+    logger.info('Saved the downloaded data to %s!', config['local_fname'])
+
+    conn = sqlite3.connect(config['db_name'])
+
+    # Create the table if it doesn't already exist
+    with open(config['create_script']) as create_statement:
+        conn.executescript(create_statement.read())
+        conn.commit()
+
+    logs.to_sql('raw_logs', con=conn, if_exists='replace', index=False)
+
+    with open(config['insert_script']) as insert_statement:
+        conn.execute(insert_statement.read())
+        conn.commit()
+
+    conn.close()
+
+    logger.info('Successfully updated %s!', config['db_name'])
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    df, file_id = download_file(client=session_client,
+                                user=app_user)
+    update_table(logs=df)
+
