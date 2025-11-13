b1e513da868269b808b9bbaa4264835a1a631931
Wilson Lui <wl2522@columbia.edu>
2025-11-13 00:08:22 -0500

    migrated box API functions from the `load_coffee_logs.py` script to the `box_utils.py` module

--- load_coffee_logs.py
+++ load_coffee_logs.py
@@ -1,21 +1,21 @@
 """Data pipeline for inserting coffee brewing logs into a SQLite3 database."""
+import functools
 import logging
 import re
 import sys
 import json
 import atexit
 import sqlite3
-import traceback
 
 import numpy as np
 import pandas as pd
 import requests
-import pytz
 
 from yaml import load, SafeLoader
 from box_sdk_gen import BoxClient, BoxJWTAuth, JWTConfig
-from box_sdk_gen.box.errors import BoxAPIError
-from box_sdk_gen.managers.uploads import PreflightFileUploadCheckParent
+
+from box_utils import (catch_exception, get_file_id, download_file,
+                       upload_log_file)
 
 
 with open('config.yml', encoding='utf-8') as config_file:
@@ -48,168 +48,6 @@ app_user = session_client.users.get_user_by_id(
 
 main_logger.info('Successfully authenticated as the Box API app user "%s"!',
                  app_user.name)
-
-
-def catch_exception(err_type, value, trace):
-    """Report any exceptions that were raised during the pipeline run."""
-    logger = logging.getLogger(__name__ + '.catch_exception')
-
-    # Extract the error message from the exception traceback
-    error_msg = traceback.format_exception_only(err_type, value)[0]
-
-    fail_notif = {"status": "FAIL",
-                  "message": f"Failed to update {config['db_name']}!",
-                  "error": error_msg.replace('\n', '')
-                  }
-    fail_msg = f'"{DATESTAMP}": `{str(fail_notif)}`'
-
-    # Report the exception with a Slack message
-    requests.post(url=SLACK_URL,
-                  data=json.dumps({'text': fail_msg}),
-                  headers={"Content-type": "application/json",
-                           "Accept": "text/plain"},
-                  timeout=config['request_timeout'])
-
-    # Log the exception and update the log file on Slack
-    logger.error('Pipeline failed with error: %s',
-                 error_msg)
-    upload_log_file(client=session_client,
-                    user=app_user,
-                    folder_id=config['folder_id'],
-                    file_id=config['log_file_id'],
-                    log_fname=config['logging_fname'])
-
-    raise err_type
-
-
-def get_file_id(client, user_id, folder_id):
-    """Get the file ID associated with each day's log file.
-
-    Since the Box API doesn't support downloading a file
-    by providing the file's path, the log file's ID needs
-    to be provided instead.
-
-    As a workaround, the `folder.preflight_check()` method is used to get
-    the log file ID. This method is meant to be used prior to uploading a
-    file to Box to check if that file already exists at the upload destination.
-
-    This method can be used to check if the log file already exists with the
-    expected file name and parent folder.
-
-    If this check fails (and returns an error), then that
-    indicates that the file already exists. The error
-    returned by the Box API will contain the file ID associated
-    with the log file, which can then be used to actually download
-    the file.
-
-    Parameters
-    ----------
-    client : box_sdk_gen BoxClient
-    user_id : str
-    folder_id : str
-
-    Returns
-    -------
-    file_id : str
-        The file ID that was assigned to the log file by Box
-
-    """
-    logger = logging.getLogger(__name__ + '.check_for_file')
-
-    # Check if the latest log file was exported to Box from the coffee.guru app
-    fname_date_suffix = f"_{pd.to_datetime('today').strftime('%d%m%Y')}.csv"
-    fname = config['local_fname'].replace('.csv', fname_date_suffix)
-
-    logger.info('Checking folder ID %s for file %s',
-                folder_id,
-                fname)
-
-    # Impersonate the account user so that all files and folders are visible
-    # (https://github.com/box/box-python-sdk/issues/466#issuecomment-557851831)
-    try:
-        # Check if the file is already in the folder
-        client.with_as_user_header(
-            user_id=user_id
-        ).uploads.preflight_file_upload_check(
-            name=fname,
-            parent=PreflightFileUploadCheckParent(id=folder_id),
-            size=None
-        )
-
-        # Raise an error if the preflight check succeeds (file doesn't exist)
-        err_msg = f"Box folder doesn't contain any file named {fname}!"
-        logger.exception(err_msg)
-
-        raise RuntimeError(err_msg)
-
-    # Return the file ID if the file already exists in the folder
-    except BoxAPIError as e:
-        logger.info('Found file "%s" in folder ID %s!',
-                    fname,
-                    folder_id)
-
-        file_id = e.response_info.body['context_info']['conflicts']['id']
-
-        logger.info('Found file ID %s associated with file name %s!',
-                    file_id,
-                    fname)
-
-        return file_id
-
-
-def download_file(client, user_id, file_id):
-    """Download the log file that should be uploaded daily.
-
-    Parameters
-    ----------
-    client : boxsdk Client
-    user_id : str
-    file_id : str
-
-    Returns
-    -------
-    logs : pandas DataFrame
-        The coffee brewing log data downloaded from the Box folder
-
-    """
-    logger = logging.getLogger(__name__ + '.download_file')
-
-    # Get the file's metadata for logging purposes
-    log_file = client.with_as_user_header(
-        user_id=config['user_id']
-    ).files.get_file_by_id(
-        file_id
-    )
-
-    upload_time = pd.to_datetime(log_file.created_at,
-                                 utc=True)
-
-    # Convert the UTC timestamp to EST or EDT
-    upload_time = upload_time.tz_convert(pytz.timezone(config['time_zone']))
-
-    logger.info('Downloading file "%s" (file ID: %s), uploaded to Box at %s',
-                log_file.name,
-                file_id,
-                str(upload_time))
-
-    # Download the matching search result using the log file ID
-    with open(config['local_fname'], mode='wb') as log_stream:
-        client.with_as_user_header(
-            user_id=user_id
-        ).downloads.download_file_to_output_stream(
-            file_id,
-            log_stream
-        )
-
-    logger.info('Downloaded file ID: %s as "%s"!',
-                file_id,
-                config['local_fname'])
-
-    logs = pd.read_csv(config['local_fname'],
-                       sep=';',
-                       iterator=False)
-
-    return logs
 
 
 def check_nan_values(logs):
@@ -478,47 +316,30 @@ def update_table(logs):
     logger.info('Successfully updated %s!', config['db_name'])
 
 
-def upload_log_file(client, user, folder_id, file_id, log_fname):
-    """Update the copy of the logging file stored in the Box folder."""
-    logger = logging.getLogger(__name__ + '.upload_log_file')
-
-    try:
-        logger.info('Updating existing log file %s in Box folder %s...',
-                    file_id,
-                    folder_id)
-
-        client.as_user(user).file(file_id).update_contents(log_fname)
-
-    except BoxAPIError as e:
-        if e.message == 'Not Found':
-            logger.warning('Log file missing from folder %s! '
-                           'Attempting to upload the log as a new file... '
-                           '(Check for a Slack notification containing the '
-                           'file ID of the newly uploaded log file)',
-                           folder_id)
-
-            file = client.as_user(user).folder(folder_id).upload(log_fname)
-
-            upload_msg = (f'"{DATESTAMP}": `Uploaded {log_fname} to Box '
-                          f"folder {folder_id} with new file ID: {file.id}! "
-                          "Remember to update the config file!`")
-            requests.post(url=SLACK_URL,
-                          data=json.dumps({'text': upload_msg}),
-                          headers={"Content-type": "application/json",
-                                   "Accept": "text/plain"},
-                          timeout=config['request_timeout'])
-
 
 if __name__ == '__main__':
-    sys.excepthook = catch_exception
+    sys.excepthook = functools.partial(
+        catch_exception,
+        config=config,
+        client=session_client,
+        user=app_user,
+        timestamp=DATESTAMP
+    )
 
-    log_file_id = get_file_id(client=session_client,
+    # Check if the latest log file was exported to Box from the coffee.guru app
+    fname_date_suffix = f"_{pd.to_datetime('today').strftime('%d%m%Y')}.csv"
+    fname = config['local_fname'].replace('.csv', fname_date_suffix)
+
+    log_file_id = get_file_id(file_name=fname,
+                              client=session_client,
                               user_id=config['user_id'],
-                              folder_id=config['folder_id'])
+                              folder_id=config['folder_id'],
+                              config=config)
 
     df = download_file(client=session_client,
                        user_id=config['user_id'],
-                       file_id=log_file_id)
+                       file_id=log_file_id,
+                       config=config)
     df = preprocess_data(logs=df)
     update_table(logs=df)
 
